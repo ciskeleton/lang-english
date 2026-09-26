246dd160636e2c396ba7db049081f613cdb69fd3
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-26 10:23:23 +0100

    Squashing Commit!

---
+++ src/info.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * CiSkeleton Language Metadata - English
+ *
+ * @package     CiSkeleton\Core
+ * @subpackage  Language
+ * @author      Kader Bouyakoub <bkader[at]mail[dot]com>
+ * @copyright   Copyright (c) 2018-present, Kader Bouyakoub
+ */
+
+return [
+	'name'       => 'English',
+	'name_en'    => 'English',
+	'locale'     => 'en_US',
+	'tag'        => 'en-US',
+	'direction'  => 'ltr',
+	'code'       => 'en',
+	'flag'       => 'us',
+	'version'    => '1.0.0',
+	'version_id' => 10000,
+
+	// Localized language names.
+	'localized_names' => [
+		'arabic'     => 'Arabic',
+		'armenian'   => 'Armenian',
+		'bengali'    => 'Bengali',
+		'brazilian'  => 'Portuguese (Brazil)',
+		'bulgarian'  => 'Bulgarian',
+		'catalan'    => 'Catalan',
+		'chinese'    => 'Chinese (Simplified)',
+		'croatian'   => 'Croatian',
+		'czech'      => 'Czech',
+		'danish'     => 'Danish',
+		'dutch'      => 'Dutch',
+		'english'    => 'English',
+		'estonian'   => 'Estonian',
+		'filipino'   => 'Filipino',
+		'finnish'    => 'Finnish',
+		'french'     => 'French',
+		'georgian'   => 'Georgian',
+		'german'     => 'German',
+		'greek'      => 'Greek',
+		'hindi'      => 'Hindi',
+		'hungarian'  => 'Hungarian',
+		'indonesian' => 'Indonesian',
+		'italian'    => 'Italian',
+		'japanese'   => 'Japanese',
+		'korean'     => 'Korean',
+		'latvian'    => 'Latvian',
+		'lithuanian' => 'Lithuanian',
+		'malay'      => 'Malay',
+		'norwegian'  => 'Norwegian',
+		'persian'    => 'Persian',
+		'polish'     => 'Polish',
+		'portuguese' => 'Portuguese',
+		'romanian'   => 'Romanian',
+		'russian'    => 'Russian',
+		'serbian'    => 'Serbian',
+		'slovak'     => 'Slovak',
+		'slovenian'  => 'Slovenian',
+		'spanish'    => 'Spanish',
+		'swahili'    => 'Swahili',
+		'swedish'    => 'Swedish',
+		'taiwanese'  => 'Chinese (Traditional)',
+		'tamil'      => 'Tamil',
+		'thai'       => 'Thai',
+		'turkish'    => 'Turkish',
+		'ukrainian'  => 'Ukrainian',
+		'urdu'       => 'Urdu',
+		'vietnamese' => 'Vietnamese',
+	],
+];
